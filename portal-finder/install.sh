285ae2676016cde70508046e103da9e072a9af11
NikoboiNFTB <github@nikoboi.dev>
2026-09-15 15:28:23 +0300

    Apparently install can't use substitution

--- portal-finder/install.sh
+++ portal-finder/install.sh
@@ -4,7 +4,12 @@ set -e
 
 echo "sudo is needed to install into /usr/local/bin/"
 
-sudo install -m 755 <(wget -qO- https://valheim.nikoboi.dev/portal-finder/script.sh) /usr/local/bin/valheim_portal_finder
-sudo install -m 755 <(wget -qO- https://valheim.nikoboi.dev/portal-finder/script.py) /usr/local/bin/valheim_portal_finder.py
+wget -qO- https://valheim.nikoboi.dev/portal-finder/script.sh |
+	sudo tee /usr/local/bin/valheim_portal_finder >/dev/null
+sudo chmod 755 /usr/local/bin/valheim_portal_finder
+
+wget -qO- https://valheim.nikoboi.dev/portal-finder/script.py |
+	sudo tee /usr/local/bin/valheim_portal_finder.py >/dev/null
+sudo chmod 755 /usr/local/bin/valheim_portal_finder.py
 
 echo "Successfully installed script.sh and script.py as valheim_portal_finder and valheim_portal_finder.py, respectively."
